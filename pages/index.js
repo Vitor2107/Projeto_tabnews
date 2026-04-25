7195fddf4c1649de162d8e027ddd8ec45096383a
Vitor de camin costa <146156924+Vitor2107@users.noreply.github.com>
2026-04-25 15:42:13 +0000

    Atualização do texto de aprezentação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function home() {
-  return <h1>Clone TabNews !!</h1>;
+  return (
+    <div>
+      <h1>Site da Andressa Cirilo Nutricionista!!</h1>
+      <h2>
+        Olá, seja bem-vindo ao meu site! Sou a Andressa Cirilo, nutricionista
+        apaixonada por ajudar as pessoas a alcançarem uma vida mais saudável e
+        equilibrada. Aqui, você encontrará dicas de alimentação, receitas
+        saudáveis e informações sobre nutrição para melhorar sua qualidade de
+        vida. Fique à vontade para explorar o site e entrar em contato comigo
+        para agendar uma consulta ou tirar dúvidas. Juntos, podemos alcançar
+        seus objetivos de saúde e bem-estar!
+      </h2>
+    </div>
+  );
 }
 
 export default home;
